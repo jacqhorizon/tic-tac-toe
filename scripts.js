e3c72f6f02a1982b38b6b1b8593e1407b1691d62
Jacq Kirkman <jacqkirkman@gmail.com>
2024-07-16 22:01:06 -0700

    change to classes

--- scripts.js
+++ scripts.js
@@ -1,91 +1,106 @@
-var xTurn = true
+// var xTurn = true
 var winner = ''
+let game
 
 function startGame() {
+  game = new Game()
+  game.board.forEach((value, i) => {
+    var id = 'c' + i
+    document.getElementById(id).value = value
+    document.getElementById(id).disabled = false
+  })
+  document
+  .getElementById(game.currentTurn.toLowerCase() + '-turn')
+  .classList.add('current-turn')
   document.getElementById('title-screen').style.display = 'none'
+  document.getElementById('end-screen').style.display = 'none'
   document.getElementById('game-screen').style.display = 'block'
 }
 
-function move(id) {
-  var value
-  if (xTurn) {
-    value = 'X'
-  } else {
-    value = 'O'
-  }
-  document.getElementById(id).value = value
-  document.getElementById(id).disabled = true
-  xTurn = !xTurn
-
-  if (xTurn) {
-    document.getElementById('x-turn').classList.add('current-turn')
-    document.getElementById('o-turn').classList.remove('current-turn')
-  } else {
-    document.getElementById('o-turn').classList.add('current-turn')
-    document.getElementById('x-turn').classList.remove('current-turn')
-  }
-}
-
-function evaluateGame() {
-  var arr = []
-  for (let i = 0; i < 9; i++) {
-    var id = 'b' + i
-    arr.push(document.getElementById(id).value)
-  }
-  var value
-  if (xTurn) {
-    value = 'OOO'
-    winner = 'O'
-  } else {
-    value = 'XXX'
-    winner = 'X'
-  }
-  let winFound = false
-  let winMap = [
-    //horizontals
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    //verticals
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    //diagonals
-    [0, 4, 8],
-    [6, 4, 2]
-  ]
-
-  for (let x = 0; x < winMap.length && !winFound; x++) {
-    let sum = ''
-    for (let i = 0; i < winMap[x].length; i++) {
-      sum += arr[winMap[x][i]]
-    }
-    if (sum === value) {
-      winFound = true
-      document.getElementById('end-text').innerHTML = winner + ' wins'
-      document.getElementById('end-screen').style.display = 'flex'
-    }
-  }
-  if (!winFound && !arr.includes('')) {
-    document.getElementById('end-text').innerHTML = 'Tie!'
-    document.getElementById('end-screen').style.display = 'grid'
-  }
-}
-
-function reset() {
-  for (let i = 0; i < 9; i++) {
-    var id = 'b' + i
-    document.getElementById(id).value = ''
-    document.getElementById(id).disabled = false
-  }
-  xTurn = true
-  document.getElementById('end-screen').style.display = 'none'
-  document.getElementById('x-turn').classList.add('current-turn')
-  document.getElementById('o-turn').classList.remove('current-turn')
-}
+// function reset() {
+//   for (let i = 0; i < 9; i++) {
+//     var id = 'b' + i
+//     document.getElementById(id).value = ''
+//     document.getElementById(id).disabled = false
+//   }
+//   xTurn = true
+//   document.getElementById('end-screen').style.display = 'none'
+//   document.getElementById('x-turn').classList.add('current-turn')
+//   document.getElementById('o-turn').classList.remove('current-turn')
+// }
 
 function toTitle() {
-  reset()
   document.getElementById('title-screen').style.display = 'flex'
+  document.getElementById('end-screen').style.display = 'none'
   document.getElementById('game-screen').style.display = 'none'
+}
+
+class Game {
+  constructor() {
+    this.board = []
+    for (let i = 0; i < 9; i++) {
+      this.board.push('')
+    }
+    this.currentTurn = 'X'
+  }
+
+  move(turn, position) {
+    console.log('move!')
+    //Place move on board
+    document
+      .getElementById(this.currentTurn.toLowerCase() + '-turn')
+      .classList.remove('current-turn')
+    this.board[position[1]] = this.currentTurn
+    document.getElementById(position).value = this.currentTurn
+    document.getElementById(position).disabled = true
+    if (this.win(this.currentTurn)) {
+      this.endGame()
+    } else {
+      //Set the next turn
+      let nextTurn = 'X'
+      if (this.currentTurn == 'X') {
+        nextTurn = 'O'
+      }
+      this.currentTurn = nextTurn
+      document
+        .getElementById(nextTurn.toLowerCase() + '-turn')
+        .classList.add('current-turn')
+    }
+    console.log(this.board)
+  }
+
+  win(turn) {
+    let winFound = false
+    let winMap = [
+      //horizontals
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      //verticals
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      //diagonals
+      [0, 4, 8],
+      [6, 4, 2]
+    ]
+    for (let x = 0; x < winMap.length && !winFound; x++) {
+      let sum = ''
+      for (let y = 0; y < winMap[x].length; y++) {
+        sum += this.board[winMap[x][y]]
+      }
+      if (sum == this.currentTurn.repeat(3)) {
+        return true
+      }
+      // if (!this.board.includes('')) {
+
+      // }
+    }
+    return false
+  }
+
+  endGame() {
+    document.getElementById('end-text').innerHTML = this.currentTurn + ' wins'
+    document.getElementById('end-screen').style.display = 'flex'
+  }
 }
